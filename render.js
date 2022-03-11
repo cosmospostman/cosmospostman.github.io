a50be1afd92b945805ef8d9e176bb6d662a0a6d0
cosmospostman <buck68@gmail.com>
2022-03-11 16:33:59 +1000

    Sound show

--- render.js
+++ render.js
@@ -1,6 +1,31 @@
 const nunjucks = require('nunjucks');
 const fs = require('fs');
+const path = require('path');
+
+const outputPath = 'html/';
 
 nunjucks.configure('src', { autoescape: true });
-rendered = nunjucks.render('index.njk');
-fs.writeFileSync('html/index.html', rendered);
+
+function render(templateName) {
+    fs.writeFileSync(
+        outputPath + templateName + '.html',
+        nunjucks.render(templateName + '.njk')
+    );
+}
+
+function renderProject(templateName) {
+    dirname = path.dirname(templateName);
+    fs.mkdirSync(outputPath + dirname, { recursive: true });
+    fs.writeFileSync(
+        outputPath + templateName + '.html',
+        nunjucks.render(templateName + '.njk')
+    );
+}
+
+render('index');
+renderProject('projects/flight');
+renderProject('projects/magnets');
+renderProject('projects/market');
+renderProject('projects/radio');
+renderProject('projects/security');
+renderProject('projects/sound');
